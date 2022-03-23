5e2f4f916fee2c7b13f4d2deb46e26adfc8f9463
Akash52 <ac8572611@gmail.com>
2022-03-23 11:50:25 +0530

    Header Component

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <h1>Task Tracker</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header-Component'
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+  padding: 20px;
+}
+</style>
